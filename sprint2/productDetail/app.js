89653727eedfeefb9f12f6cf22f79c2588e6025e
33lucasant <lucassantgomez@gmail.com>
2023-11-11 16:54:56 -0300

    Agregando la carpeta sprint2 con productDetail

---
+++ sprint2/productDetail/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3000, () => console.log('Servidor funcionando en el puerto 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});

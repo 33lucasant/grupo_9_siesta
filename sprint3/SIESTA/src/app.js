39921a052a9d5fca5ea7916a24bf4517b5066e9c
33lucasant <lucassantgomez@gmail.com>
2023-11-27 16:38:50 -0300

    Subiendo los formularios de creación y edición de productos

--- sprint3/SIESTA/src/app.js
+++ sprint3/SIESTA/src/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const main = require('./routers/main')
-const products = require('./routers/products')
+const main = require('./routers/main');
+const products = require('./routers/products');
+const bodyParser = require('body-parser');
+const methodOverride = require("method-override");
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
 app.use('/', main);
 app.use('/products', products);
+
+app.use((req, res, next) => {
+    res.status(404).render('error404')
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor funcionando en http://localhost:${PORT}`));
--- sprint3/SIESTA/src/routers/products.js
+++ sprint3/SIESTA/src/routers/products.js
@@ -5,7 +5,9 @@ const productsController = require('../controllers/productsController');
 router.get('/', productsController.products);
 router.get('/product/:id', productsController.detail);
 router.get('/cart', productsController.cart);
-router.get('/create', productsController.create);
-router.get('/edit', productsController.edit);
+router.get('/create', productsController.createProduct);
+router.post('/create', productsController.addProduct);
+router.get('/edit/:productID', productsController.edit);
+router.put('/edit', productsController.editProduct);
 
 module.exports = router;

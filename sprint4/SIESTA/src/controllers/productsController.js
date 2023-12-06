b8db9dc05e682bdd24756e563e4f3d775a94e6e5
33lucasant <lucassantgomez@gmail.com>
2023-12-06 16:34:58 -0300

    Agregando multer

--- sprint4/SIESTA/src/controllers/productsController.js
+++ sprint4/SIESTA/src/controllers/productsController.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const productsController = {
     products: (req, res) => {
@@ -12,47 +14,75 @@ const productsController = {
     detail: (req, res) => {
         const productID = req.params.id;
 		const productDetail = products.find(product => product.id == productID);
-		res.render('productDetail', {productDetail})
+		res.render('productDetail', {productDetail});
     },
 
     createProduct: (req, res) => {
-        res.render('create')
+        res.render('create');
     },
 
     addProduct: (req, res) => {
-        products.push({
-            id: products.length + 1,
-            name: req.body.name,
-            price: req.body.price,
-            image: req.body.image,
-            description: req.body.description,
-            colors: req.body.colors,
-            size1: req.body.size1,
-            size2: req.body.size2,
-            size3: req.body.size3,
-            size4: req.body.size4
-        });
-        res.redirect('/products');
+        try {
+            let productImages = [];
+            for (let i = 0; i < 6; i++) {
+                productImages[i] = req.files[i].filename
+            }
+
+			const newProduct = {id: products.length + 1, ...req.body, images: productImages}
+			products.push(newProduct);
+
+			res.redirect('/products');
+
+            fs.writeFileSync(productsFilePath, JSON.stringify(products));
+		} catch (error) {
+			console.log('error:', error);
+		}
     },
     
     edit: (req, res) => {
-        res.render('edit')
+        const productID = req.params.id;
+		const productToEdit = products.find(product => product.id == productID);
+        res.render('edit', {productToEdit});
     },
 
     editProduct: (req, res) => {
-        let productToEdit = products.find(product => product.id == req.params.id);
-        productToEdit = {
-            name: req.body.name,
-            price: req.body.price,
-            image: req.body.image,
-            description: req.body.description,
-            colors: req.body.colors,
-            size1: req.body.size1,
-            size2: req.body.size2,
-            size3: req.body.size3,
-            size4: req.body.size4
-        };
-        res.render(res.redirect("/products"), {productToEdit});
+        const productID = req.params.id;
+
+        let productImages = [];
+        for (let i = 0; i < 6; i++) {
+            productImages[i] = req.files[i].filename
+        }
+
+        const {name, price, description, colors, size1, size2, size3, size4} = req.body;
+
+        const indexProd = products.findIndex(product => product.id == productID);
+
+        if (indexProd != -1) {
+            products[indexProd].name = name;
+			products[indexProd].price = price;
+			products[indexProd].description = description;
+			products[indexProd].colors = colors;
+			products[indexProd].size1 = size1;
+            products[indexProd].size2 = size2;
+            products[indexProd].size3 = size3;
+            products[indexProd].size4 = size4;
+            products[indexProd].images = productImages;
+
+            fs.writeFileSync(productsFilePath, JSON.stringify(products));
+
+            res.redirect('/products');
+        } else {
+            res.send('PRODUCTO NO ENCONTRADO');
+        }
+    },
+
+    delete: (req, res) => {
+        const productID = req.params.id;
+		products = products.filter(product => product.id != productID);
+
+		res.redirect('/products');
+        
+        fs.writeFileSync(productsFilePath, JSON.stringify(products));
     },
 
     cart: (req, res) => {
--- sprint4/SIESTA/src/routers/products.js
+++ sprint4/SIESTA/src/routers/products.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/multer');
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.products);
-router.get('/product/:id', productsController.detail);
+
+router.get('/detail/:id', productsController.detail);
+
 router.get('/cart', productsController.cart);
+
 router.get('/create', productsController.createProduct);
-router.post('/create', productsController.addProduct);
-router.get('/edit/:productID', productsController.edit);
-router.put('/edit', productsController.editProduct);
+router.post('/', upload.array('images', 6), productsController.addProduct);
+
+router.get('/edit/:id', productsController.edit);
+router.put('/:id', upload.array('images', 6), productsController.editProduct);
+
+router.delete('/:id', productsController.delete); 
 
 module.exports = router;
